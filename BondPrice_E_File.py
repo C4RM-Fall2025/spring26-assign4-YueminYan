a7d40d316e3290ec3b7de92babe084c64dd6d88c
YueminYan <yy3674@columbia.edu>
2026-02-19 17:43:06 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,3 +1,11 @@
-
+def getBondPrice_E(face,couponRate,m,yc):
+  sumpvcf=0
+  cf=face*couponRate
+  for t, yt in enumerate(yc,start=1):
+    pvf=(1+yt)**-t
+    if t==m:
+      cf=cf+face
+    sumpvcf=sumpvcf+cf*pvf
+  return sumpvcf
 
 
