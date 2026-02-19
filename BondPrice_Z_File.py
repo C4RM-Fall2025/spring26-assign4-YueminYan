00d1a13237a5262bcd1908955cf1de2145b574b9
YueminYan <yy3674@columbia.edu>
2026-02-19 17:43:51 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,11 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+  sumpvcf=0
+  cf=face*couponRate
+  for t, y in zip(times,yc):
+    pvf=(1+y)**-t
+    if t==times[-1]:
+      cf=cf+face
+    sumpvcf=sumpvcf+cf*pvf
+  return sumpvcf
