3732e93c17df2f3ea0d4983ee08aee9f12a2e49e
YueminYan <yy3674@columbia.edu>
2026-02-19 17:40:14 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,20 @@
 
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+def getBondprice(y,face,couponRate,m,ppy):
+  sumpvcf=0
+  cf=face*couponRate/ppy
+  if ppy==1:
+    if i==m:
+      cf=cf+face
+    for i in range(1,m+1):
+      pvf=(1+y)**-i
+      sumpvcf=sumpvcf+cf*pvf
+  elif ppy!=1:
+    t=m*ppy
+    for j in range(1,t+1):
+      if j==m*ppy:
+        cf=cf+face
+      for j in range(1,m+1):
+        pvf=(1+y)**-j
+        sumpvcf=sumpvcf+cf*pvf
+  return sumpvcf
