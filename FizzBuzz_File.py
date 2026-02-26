28b5510d7b27a726dc81e0c14a2e656cc15b28b7
YueminYan <yy3674@columbia.edu>
2026-02-26 18:59:43 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,16 +1,13 @@
-
-
 def FizzBuzz(start,end):
   outlist=[]
   for i in range(start,end+1):
     if i % 15==0:
-      outlist.append('FizzBuzz')
+      outlist.append('fizzbuzz')
     elif i % 3==0:
-      outlist.append('Fizz')
+      outlist.append('fizz')
     elif i % 5==0:
-      outlist.append('Buzz')
+      outlist.append('buzz')
     else:
       outlist.append(i)
 
-  return outlist
-
+   return outlist
