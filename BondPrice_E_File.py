2c36420a34e51b005db38d537c4f8a051bd19492
YueminYan <yy3674@columbia.edu>
2026-02-26 19:38:33 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,9 +1,16 @@
-def getBondPrice_E(face,couponRate,m,yc):
-  sumpvcf=0
-  cf=face*couponRate
-  for t, yt in enumerate(yc,start=1):
-    pvf=(1+yt)**-t
-    if t==m:
-      cf=cf+face
-    sumpvcf=sumpvcf+cf*pvf
-  return round(sumpvcf)
+def getBondPrice_E(face, couponRate, yc):
+
+    cf = face * couponRate
+    price = 0
+    m = len(yc)
+
+    for t, yt in enumerate(yc, start=1):
+
+        if t == m:
+            cashflow = cf + face
+        else:
+            cashflow = cf
+
+        price += cashflow / (1 + yt)**t
+
+    return price
